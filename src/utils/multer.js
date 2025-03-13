f0e7e586cc6456b6581ce3c8f30342615e5efc03
Amon Sharma <159574376+AmonSharma2004@users.noreply.github.com>
2025-03-13 15:20:50 +0530

    tempDirectory

--- src/utils/multer.js
+++ src/utils/multer.js
@@ -3,9 +3,9 @@ import multer from 'multer';
 import path from 'path';
 
 // Ensure 'uploads' folder exists
-const uploadDir = 'uploads/';
+const uploadDir = '/tmp/uploads';
 if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir, { recursive: true }); // Creates the folder if it doesn't exist
+  fs.mkdirSync(uploadDir, { recursive: true });
 }
 
 // Storage configuration
