313c0781792cceecc51117a917f1c408f75ef089
Amon Sharma <111745899+Amon20044@users.noreply.github.com>
2025-03-16 01:33:15 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,6 @@ dotenv.config();
 const app = express(); // Declare app
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173", // Allow only your frontend URL
     credentials: true, // Allow cookies if needed
   })
 );
